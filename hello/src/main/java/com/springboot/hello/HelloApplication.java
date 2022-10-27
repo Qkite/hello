04d0a9906fbd1608e72c14e64e08beb8da9a019e
Qkite <yeonjigu4@gmail.com>
2022-10-27 15:34:02 +0900

    swagger 추가 - Hello application의 nullpointException 해결

--- hello/src/main/java/com/springboot/hello/HelloApplication.java
+++ hello/src/main/java/com/springboot/hello/HelloApplication.java
@@ -8,7 +8,14 @@ public class HelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
-		// nullpointerexception 발생
+		// nullpointerexception 발생 -> application.yml에 설정 변경하기
+//		server:
+//		port: 8081
+//
+//		spring:
+//		mvc:
+//		pathmatch:
+//		matching-strategy: ant_path_matcher
 	}
 
 }
