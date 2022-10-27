67b1f831789b1edeb8ddca4ea559be8414453481
Qkite <yeonjigu4@gmail.com>
2022-10-27 13:15:13 +0900

    MemberDto 클래스로 정보 받기

--- hello/src/main/java/com/springboot/hello/controller/HelloController.java
+++ hello/src/main/java/com/springboot/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.springboot.hello.controller;
 
+import com.springboot.hello.domain.dto.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -32,12 +33,12 @@ public class HelloController {
 
 
     @GetMapping(value = "/request1")
-    public String getParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
+    public String getRequestParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
         return String.format("%s %s %s", name, email, organization);
     }
 
     @GetMapping(value = "/request2")
-    public String getParam2(@RequestParam Map<String, String> param){
+    public String getRequestParam2(@RequestParam Map<String, String> param){
         param.entrySet().forEach(map -> {
             System.out.printf("key:%s value:%s\n", map.getKey(), map.getValue());
         } );
@@ -45,6 +46,12 @@ public class HelloController {
         return "request2가 호출 완료 되었습니다";
         // Java8의 Stream 형식
         //(mpa -> {}) 이 부분이 메소드의 형식이라고 생각하면 됨
+    }
+
+    @GetMapping(value = "/request3")
+    public String getRequestParam3(MemberDto memberDto){
+        System.out.println(memberDto);
+        return "request3 호출 완료";
     }
 
 
