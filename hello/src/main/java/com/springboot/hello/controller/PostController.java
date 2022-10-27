d7995b7ad6d7b5ec8a9156f6b820367bed17be77
Qkite <yeonjigu4@gmail.com>
2022-10-27 14:15:47 +0900

    postmapping memberdto로 정보 출력하기

--- hello/src/main/java/com/springboot/hello/controller/PostController.java
+++ hello/src/main/java/com/springboot/hello/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.springboot.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -10,5 +11,22 @@ public class PostController {
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample(){
         return "Hello Post API";
+    }
+
+    @PostMapping(value = "/member1")
+    // request body를 사용함
+    public String postMember1(@RequestBody Map<String, Object> postData){
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMember2(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
     }
 }
