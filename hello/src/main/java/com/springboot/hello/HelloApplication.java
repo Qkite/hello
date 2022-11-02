053d76c6b30f0b58397de539494c63b5370fc989
Qkite <yeonjigu4@gmail.com>
2022-11-02 14:26:41 +0900

    test코드 주석 처리, HelloApplication main 주석처리

--- hello/src/main/java/com/springboot/hello/HelloApplication.java
+++ hello/src/main/java/com/springboot/hello/HelloApplication.java
@@ -6,16 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
-		// nullpointerexception 발생 -> application.yml에 설정 변경하기
-//		server:
-//		 port: 8081
+//	public static void main(String[] args) {
+//		SpringApplication.run(HelloApplication.class, args);
+//		// nullpointerexception 발생 -> application.yml에 설정 변경하기
+////		server:
+////		 port: 8081
+////
+////		spring:
+////		 mvc:
+////		  pathmatch:
+////		   matching-strategy: ant_path_matcher
+//	}
 //
-//		spring:
-//		 mvc:
-//		  pathmatch:
-//		   matching-strategy: ant_path_matcher
-	}
-
-}
+//}
